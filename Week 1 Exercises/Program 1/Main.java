8d35860f0757f149c2b7e51d9f1be8bad54aeef5
SamEldridge100145810 <s.eldridge@burnley.ac.uk>
2022-09-26 10:21:49 +0100

    Addition

--- Week 1 Exercises/Program 1/Main.java
+++ Week 1 Exercises/Program 1/Main.java
@@ -3,5 +3,6 @@ class Main
     public static void main(String[] args)
     {
         System.out.println("Hello World 3");
+        System.out.println("Hey whats up guys, Quandale Dingle here.");
     }
 }
