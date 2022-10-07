9db73ee92e1bc26452220b47d6f31a5c9b8715e0
SamEldridge100145810 <s.eldridge@burnley.ac.uk>
2022-10-07 09:48:01 +0100

    more changes

--- Week 1 Exercises/Program 1/Main.java
+++ Week 1 Exercises/Program 1/Main.java
@@ -3,7 +3,5 @@ class Main
     public static void main(String[] args)
     {
         System.out.println("Hello World 3");
-        System.out.println("Hey whats up guys, Quandale Dingle here.");
-        System.out.println("THE ONE PIECE... THE ONE PIECE IS REAL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
